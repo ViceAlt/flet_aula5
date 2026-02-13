f6234a8010161a5b2b6dd1c3d366bac1d5d32ceb
Vito Virito <vitor.landin@aluno.senai.br>
2026-02-13 11:55:02 -0300

    final
    final

--- game.py
+++ game.py
@@ -1,11 +1,23 @@
 import flet as ft
 
 def main(page: ft.Page):
+    mensagem = ft.Text("Escolha a opção correta!")
+    resposta_correta = "Tijolo"
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "Parabens"
+        else:
+            mensagem.value = "Resposta errada"
+        page.update()
+
+
     page.title = "Game: Adivinhe a imagem"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = ft.Colors.BLACK
 
     page.add(
+        
         ft.Column(
             controls=[
                 ft.Text(
@@ -16,9 +28,34 @@ def main(page: ft.Page):
                 ft.Image(
                     src="image/gato.jpg",
                     height=200,
+                ),
+                mensagem,
+                ft.Row(
+                    controls=[
+                        ft.Button(
+                            content="Gato",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Cachorro",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Tatu",
+                            on_click=verificar_resposta
+                        )
+                    ],
                     alignment=ft.MainAxisAlignment.CENTER
-                )
-            ]
+                ),
+                ft.Button(
+                            content="Tijolo",
+                            on_click=verificar_resposta,
+                            color=ft.Colors.BLACK_12,
+                            bgcolor=ft.Colors.BLACK_12,
+                        ),
+                
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
     )
 
